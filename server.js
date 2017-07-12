51c6e7fbbafb2b2c04c1c4bb33307fe318e10bbd
Yung-Yue Fung <yfung@toasttab.com>
2017-07-11 21:32:13 -0400

    adjusted heroku url and port

--- server.js
+++ server.js
@@ -11,4 +11,6 @@ app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port);
--- test/app.js
+++ test/app.js
@@ -10,7 +10,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    connectionString += '@ds155192.mlab.com:55192/heroku_f32310bb'; // user yours
 }
 // Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
 // above with your own URL given to you by mLab
