bc88f94be2257dda805f609a44809912922f4b41
Yung-Yue Fung <yfung@toasttab.com>
2017-07-17 19:57:58 -0400

    more js

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -3,7 +3,19 @@ var app = angular.module("my-app", []);
 app.controller("loginController", loginController);
 
 function loginController($scope) {
-    $scope.login = function() {
-        alert("login clicked");
-    };
+    $scope.login = function(username, password) {
+        var users = [
+            {_id: "123", username: "alice", password: "alice"}
+        ];
+
+        $scope.login = function(user) {
+            for(var u in users) {
+                var _user = users[u];
+                if(_user.username === user.username && _user.password === user.password) {
+                    $scope.welcomeUser = _user;
+                }
+            }
+        }
+
+    }
 }
