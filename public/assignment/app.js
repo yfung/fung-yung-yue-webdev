2a366394f8cbc05c8445fb9d2f6563c71aa3f958
Yung-Yue Fung <yfung@toasttab.com>
2017-07-18 19:50:48 -0400

    more js

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -5,8 +5,12 @@ app.controller("loginController", loginController);
 app.config(configuration);
 
 var users = [
-    {_id: "123", username: "alice", password: "alice"}
-];
+    {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+    {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+    {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+]
+
 
 function configuration($routeProvider) {
     $routeProvider
