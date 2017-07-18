f10f0afe9a35b14fd54e1dc3cf3f56220654af9e
Yung-Yue Fung <yfung@toasttab.com>
2017-07-17 20:43:22 -0400

    more js

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -4,23 +4,35 @@ app.controller("loginController", loginController);
 
 app.config(configuration);
 
+var users = [
+    {_id: "123", username: "alice", password: "alice"}
+];
+
 function configuration($routeProvider) {
     $routeProvider
         .when("/login", {templateURL: "login.html"})
         .when("/register", {templateURL: "register.html"})
-        .when("/profile", {templateURL: "profile.html"});
+        // : refers to a place holder with the name userId
+        .when("/profile/:userId", {templateURL: "profile.html"});
+}
+
+function profileController($scope, $routeParams) {
+    // routeParams is a map to all parameters of possible routes we gave i.e. /login
+    var userId = $routeParams["userId"]
+    for (var u in users) {
+        if (users[u]._id === userId) {
+            $scope.user = users[u];
+        }
+    }
+
 }
 
 function loginController($scope, $location) {
-    var users = [
-        {_id: "123", username: "alice", password: "alice"}
-    ];
-
     $scope.login = function (user) {
         for (var u in users) {
             var _user = users[u];
             if (_user.username === user.username && _user.password === user.password) {
-                $location.url("profile");
+                $location.url("profile/" + _user._id);
             }
         }
         $scope.errorMessage = "Username or password was incorrect. Please try again!"
