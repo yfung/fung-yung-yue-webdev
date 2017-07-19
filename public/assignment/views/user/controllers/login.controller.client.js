cabe853d56aa0a869a45858041e7a1bb72af8299
Yung-Yue Fung <yfung@toasttab.com>
2017-07-19 19:07:51 -0400

    user service

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -5,22 +5,13 @@
         .module("my-app")
         .controller("loginController", loginController);
 
-    var users = [
-        {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-        {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-        {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-    ];
-
-    function loginController($scope, $location) {
+    function loginController($scope, $location, userService) {
         $scope.login = function (user) {
-            for (var u in users) {
-                var _user = users[u];
-                if (_user.username === user.username && _user.password === user.password) {
-                    $location.url("profile/" + _user._id);
-                }
+            if (userService.findUserByUsernameAndPassword(user.username, user.password) === null) {
+                $scope.errorMessage = "Username or password was incorrect. Please try again!"
+            } else {
+                $location.url("user/" + "");
             }
-            $scope.errorMessage = "Username or password was incorrect. Please try again!"
         }
     }
 })();
--- public/assignment/views/user/controllers/profile.controller.client.js
+++ public/assignment/views/user/controllers/profile.controller.client.js
@@ -5,14 +5,7 @@
         .module("my-app")
         .controller("profileController", profileController);
 
-    var users = [
-        {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-        {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-        {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-    ];
-
-    function profileController($scope, $routeParams) {
+    function profileController($scope, $routeParams, userService) {
         // routeParams is a map to all parameters of possible routes we gave i.e. /login
         var userId = $routeParams["userId"];
         for (var u in users) {
