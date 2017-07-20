edf01c1d0a6db6894e9a53d88d9ff14668579c13
Yung-Yue Fung <yfung@toasttab.com>
2017-07-20 16:50:33 -0400

    model as controller name

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -7,20 +7,18 @@
 
         this.findWebsitesForUser = findWebsitesForUser;
 
+        var websites = [
+            { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
+            { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
+            { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
+            { "_id": "890", "name": "Go",          "developerId": "123", "description": "Lorem" },
+            { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
+            { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
+            { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
+        ];
+
         function findWebsitesForUser(userId) {
-
-            var websites = [
-                { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
-                { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
-                { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
-                { "_id": "890", "name": "Go",          "developerId": "123", "description": "Lorem" },
-                { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
-                { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
-                { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
-            ];
-
-            function findWebsitesForUser(userId) {
-                var sites = [];
+             var sites = [];
 
                 for(var w in websites) {
                     if(websites[w].developerId === userId) {
@@ -31,6 +29,5 @@
                 return sites;
             }
         }
-    }
 
 })();
