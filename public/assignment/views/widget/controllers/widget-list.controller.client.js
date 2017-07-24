99875cc7a42507076304bbad660ef4297dbd4a8c
Yung-Yue Fung <yfung@toasttab.com>
2017-07-24 19:18:07 -0400

    youtube working

--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -3,17 +3,31 @@
         .module("yungApp")
         .controller("widgetListController", widgetListController);
 
-    function widgetListController($routeParams, widgetService) {
+    function widgetListController($routeParams, widgetService, $sce) {
         var model = this;
 
         model.websiteId = $routeParams.websiteId;
         model.userId = $routeParams.userId;
         model.pageId = $routeParams.pageId;
 
+        this.trustHTMLContent = trustHTMLContent;
+        this.trustUrlResource = trustUrlResource;
+
         function init() {
             model.widgets = widgetService.findWidgetsByPageId(model.pageId);
         }
         init();
+
+        function trustHTMLContent(htmlContent) {
+            return $sce.trustAsHtml(htmlContent);
+        }
+
+        function trustUrlResource(url) {
+            var youtubeUrl = "https://www.youtube.com/embed/";
+            var urlParts = url.split("/");
+            youtubeUrl += urlParts[urlParts.length-1];
+            return $sce.trustAsResourceUrl(youtubeUrl);
+        }
 
     }
 
