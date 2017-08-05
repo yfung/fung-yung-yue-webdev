7b2e3350d2a2c2d5e96f79d458f85ff64f6e61f6
Yung-Yue Fung <yfung@toasttab.com>
2017-08-04 20:21:11 -0400

    api lastfm

--- public/test/app.js
+++ public/test/app.js
@@ -1,19 +1,72 @@
 (function () {
-    $(init);
+    angular
+        .module("lastFM", ['ngRoute'])
+        .config(configuration)
+        .controller("trackController", trackController)
+        .controller("detailsController", detailsController);
 
-    function init() {
-        var api_key = '78d4d2baebb25b906657a9a40ff7e684';
-        var searchButton = $("#searchButton");
+    var api_key = '78d4d2baebb25b906657a9a40ff7e684';
 
-        searchButton.click(function () {
-            var songSearchField = $("#songSearchField");
+    function trackController() {
+        this.buttonSearch = buttonSearch;
+
+        function init() {
+        }
+
+        init();
+
+        function buttonSearch(search) {
             $.ajax({
                 type: 'GET',
-                url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + songSearchField.val() + '&api_key=' + api_key + '&format=json&limit=5',
+                url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + search + '&api_key=' + api_key + '&format=json&limit=5',
                 success: renderSongs
             })
-        });
+        }
+    }
 
+    function detailsController($routeParams) {
+        var model = this;
+        model.id = $routeParams.trackID;
+
+        function init() {
+            getTrack(model.id);
+        }
+
+        init();
+
+        function getTrack(id) {
+            $.ajax({
+                type: 'GET',
+                url: 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo&mbid=' + id + '&api_key=' + api_key + '&format=json&limit=5',
+            }).done(function(response) {
+                var songName = $("<div>");
+                var songArtist = $("<div>");
+                var songPlays = $("<div>");
+
+                songName.append("Track Title: " + response.track.name);
+                songArtist.append("Artist: " + response.track.artist.name);
+                songPlays.append("Total Listens: " + response.track.playcount);
+
+                $("#trackData").append(songName);
+                $("#trackData").append(songArtist);
+                $("#trackData").append(songPlays);
+
+            });
+        }
+    }
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "search.html",
+                controller: "trackController",
+                controllerAs: "model"
+            })
+            .when("/track/:trackID", {
+                templateUrl: "track.html",
+                controller: "detailsController",
+                controllerAs: "model"
+            })
     }
 
     function renderSongs(response) {
@@ -28,9 +81,14 @@
         for (var s in results) {
             var song = results[s];
             var tr = $("<tr>");
+            var a = $("<a>");
             var tdname = $("<td>");
             var tdartist = $("<td>");
-            tdname.append(song.name);
+            a.append(song.name);
+            a.attr('href', function () {
+                return this.href + "/test/#!/track/" + song.mbid;
+            });
+            tdname.append(a);
             tdartist.append(song.artist);
             tr.append(tdname);
             tr.append(tdartist);
