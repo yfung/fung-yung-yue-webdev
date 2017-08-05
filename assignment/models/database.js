7051d90a08f223bafac321f965dcbc3f2f94e420
Yung-Yue Fung <yfung@toasttab.com>
2017-08-05 18:40:23 -0400

    local db name

--- assignment/models/database.js
+++ assignment/models/database.js
@@ -1,6 +1,6 @@
 var q = require('q');
 
-var connectionString = 'mongodb://127.0.0.1:27017/fung-yung-yue-webdev';
+var connectionString = 'mongodb://127.0.0.1:27017/assignment';
 if (process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
