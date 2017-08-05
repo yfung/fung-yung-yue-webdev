23c46b1733aa0122691beabf61576006d3ffe21c
Yung-Yue Fung <yfung@toasttab.com>
2017-08-05 15:18:47 -0400

    db

---
+++ assignment/models/database.js
@@ -0,0 +1,14 @@
+var q = require('q');
+
+var connectionString = 'mongodb://127.0.0.1:27017/fung-yung-yue-webdev';
+if (process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds155192.mlab.com:55192/heroku_f32310bb';
+}
+
+var mongoose = require("mongoose");
+var db = mongoose.connect(connectionString);
+mongoose.Promise = q.Promise;
+module.exports = db;
