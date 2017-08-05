7ba2cd8cbbaa75fdd82d2b4847cfc6126d09c046
Yung-Yue Fung <yfung@toasttab.com>
2017-08-04 21:02:27 -0400

    id on details page

--- public/test/app.js
+++ public/test/app.js
@@ -50,15 +50,18 @@
                 var songName = $("<div>");
                 var songArtist = $("<div>");
                 var songPlays = $("<div>");
+                var songId = $("<div>");
 
                 songName.append("Track Title: " + response.track.name);
                 songArtist.append("Artist: " + response.track.artist.name);
                 songPlays.append("Total Listens: " + response.track.playcount);
+                songId.append("Track ID: " + response..track.mbid);
 
                 $("#trackData").append(songName);
                 $("#trackData").append(songArtist);
                 $("#trackData").append(songPlays);
-
+                $("#trackData").append(songId);
+                
             });
         }
     }
