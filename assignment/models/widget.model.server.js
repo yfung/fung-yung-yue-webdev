f1c52395a347fb4556357cfd887e94ea6c4bc5d3
Yung-Yue Fung <yfung@toasttab.com>
2017-08-06 16:12:36 -0400

    widget model

---
+++ assignment/models/widget.model.server.js
@@ -0,0 +1,6 @@
+var mongoose = require("mongoose");
+var widgetSchema = require("./widget.schema.server");
+var db = require("./database");
+var widgetModel = mongoose.model("widgetModel", widgetSchema);
+
+module.exports = widgetModel;
