af63ec9d139718f4a57ea6afe9849617d3e5fa57
Yung-Yue Fung <yfung@toasttab.com>
2017-08-06 13:22:31 -0400

    user service no data

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -1,13 +1,6 @@
 var app = require("../../express");
 var userModel = require("../models/user.model.server");
 
-var users = [
-    {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-    {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-    {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-    {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-];
-
 // All url with api are meant to return dynamic data
 app.get("/api/users", getAllUsers);
 app.get("/api/user/:userId", getUserById);
