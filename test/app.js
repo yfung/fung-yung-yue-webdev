d3b7b9ebd53f236e125f53933dcec1e04acfb606
Yung-Yue Fung <yfung@toasttab.com>
2017-08-06 21:15:23 -0400

    test db

--- test/app.js
+++ test/app.js
@@ -5,15 +5,7 @@ app.get("/api/test", findAllMessages);
 app.post("/api/test", createMessage);
 app.delete("/api/test/:id", deleteMessage);
 
-var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds155192.mlab.com:55192/heroku_f32310bb'; // user yours
-}
-// Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
-// above with your own URL given to you by mLab
+var connectionString = 'mongodb://127.0.0.1:27017/assignment'; // for local
 
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
