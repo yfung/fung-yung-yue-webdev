dcd215118bdfe9e896c9b90e90ee45a6b0772d53
Yung-Yue Fung <yfung@toasttab.com>
2017-08-11 18:03:09 -0400

    user implementation

--- assignment/models/users.model.server.js
+++ assignment/models/users.model.server.js
@@ -1,5 +1,36 @@
 var mongoose = require("mongoose");
 var usersSchema = require("./users.schema.server");
 var usersModel = mongoose.model("UsersModel", usersSchema);
+usersModel.createUser = createUser;
+usersModel.findUserById = findUserById;
+usersModel.updateUser = updateUser;
+usersModel.findUserByUsername = findUserByUsername;
+usersModel.deleteUser = deleteUser;
+usersModel.findUserByCredentials = findUserByCredentials;
 
 module.exports = usersModel;
+
+function findUserById(userId) {
+    return userModels.findById(userId);
+}
+
+function findUserByCredentials(username, password) {
+    return usersModel.findOne({username: username, password: password});
+}
+
+function findUserByUsername(username) {
+    return usersModel.findOne({username: username});
+}
+
+function createUser(user) {
+    return usersModel.create(user);
+}
+
+function updateUser(userId, user) {
+    return usersModel.update({_id: userId},
+        {$set: user});
+}
+
+function deleteUser(userId) {
+    return usersModel.remove({_id: userId});
+}
--- assignment/services/users.service.server.js
+++ assignment/services/users.service.server.js
@@ -1,2 +1,74 @@
 var app = require("../../express");
 var usersModel = require("../models/users.model.server");
+
+app.get("/api/users/:userId", getUserById);
+app.get("/api/users", findUser);
+app.post("/api/users", registerUser);
+app.put("/api/users/:userId", updateUser);
+app.delete("/api/users/:userId", deleteUser);
+
+function getUserById(request, response) {
+    usersModel
+        .findUserById(request.params.userId)
+        .then(function (user) {
+            response.json(user);
+        });
+}
+
+function findUser(request, response) {
+    var username = request.query.username;
+    var password = request.query.password;
+
+    if (username && password) {
+        usersModel
+            .findUserByCredentials(username, password)
+            .then(function (user) {
+                response.json(user);
+                return;
+            }, function (err) {
+                response.sendStatus(404).send(err);
+                return;
+            });
+        return;
+    } else if (username) {
+        usersModel.findUserByUsername(username)
+            .then(function (user) {
+                response.json(user);
+                return;
+            });
+    }
+}
+
+function registerUser(request, response) {
+    var user = request.body;
+    usersModel.createUser(user)
+        .then(function (user) {
+            response.send(user);
+        });
+}
+
+function updateUser(request, response) {
+    var userId = request.params.userId;
+    var user = request.body;
+
+    usersModel
+        .updateUser(userId, user)
+        .then(function (status) {
+            response.json(status);
+        }, function (err) {
+            response.sendStatus(404).send(err);
+        });
+}
+
+function deleteUser(request, response) {
+    var userId = request.params.userId;
+
+    usersModel
+        .deleteUser(userId)
+        .then(function () {
+            response.send("1");
+        }, function (err) {
+            response.sendStatus(404).send(err);
+        });
+
+}
