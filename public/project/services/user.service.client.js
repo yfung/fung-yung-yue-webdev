33f2eb004185bea6380cf9d983888fe0735bdc3f
Yung-Yue Fung <yfung@toasttab.com>
2017-08-11 17:40:13 -0400

    user implementation

---
+++ public/project/services/user.service.client.js
@@ -0,0 +1,43 @@
+(function () {
+    angular
+        .module("rhythmShark")
+        .factory("userService", userService);
+
+    function userService($http) {
+
+        var api = {
+            "findUserByUsername": findUserByUsername,
+            "findUserByUsernameAndPassword": findUserByUsernameAndPassword,
+            "findUserById": findUserById,
+            "registerUser": registerUser,
+            "updateUser": updateUser,
+            "deleteUser": deleteUser
+        };
+        return api;
+
+        function updateUser(userId, user) {
+            return $http.put("/api/user/" + userId, user)
+        }
+
+        function registerUser(user) {
+            return $http.post("/api/user", user);
+        }
+
+        function findUserByUsername(username) {
+            return $http.get("/api/user?username=" + username);
+        }
+
+        function findUserById(userId) {
+            return $http.get("/api/user/" + userId);
+        }
+
+        function findUserByUsernameAndPassword(username, password) {
+            return $http.get("/api/user?username=" + username + "&password=" + password);
+        }
+
+        function deleteUser(userId) {
+            return $http.delete("/api/user/" + userId);
+        }
+
+    }
+})();
