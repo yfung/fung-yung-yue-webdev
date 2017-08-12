b7aa5376e5beebea0c3a110e22ba982b70a11d73
Yung-Yue Fung <yfung@toasttab.com>
2017-08-12 17:22:44 -0400

    more playlist stuff

--- assignment/models/playlist.schema.server.js
+++ assignment/models/playlist.schema.server.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var playlistSchema = mongoose.Schema({
+    name: String,
     dateCreated: {type: Date, default: Date.now()},
     description: String,
     createdBy: {type: mongoose.Schema.Types.ObjectId, ref:"UsersModel"},
