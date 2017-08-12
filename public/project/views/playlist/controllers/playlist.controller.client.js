b3efbb5556094974ce46324d00e011b1f3812f27
Yung-Yue Fung <yfung@toasttab.com>
2017-08-12 15:08:03 -0400

    updated index.html

---
+++ public/project/views/playlist/controllers/playlist.controller.client.js
@@ -0,0 +1,16 @@
+(function () {
+    angular
+        .module("rhythmShark")
+        .controller("playlistController", playlistController);
+
+    function playlistController($location, playlistService) {
+        var model = this;
+
+        function init() {
+
+        }
+
+        init();
+    }
+
+})();
