e5cbdd873930f90c673386497aae23cbdc706144
Yung-Yue Fung <yfung@toasttab.com>
2017-08-12 20:11:50 -0400

    playlist model

--- assignment/models/playlist.model.server.js
+++ assignment/models/playlist.model.server.js
@@ -3,3 +3,7 @@ var playlistSchema = require("./playlist.schema.server");
 var playlistModel = mongoose.model("PlaylistModel", playlistSchema);
 
 module.exports = playlistModel;
+
+function createPlaylist(playlist) {
+    return playlistModel.create(playlist);
+}
