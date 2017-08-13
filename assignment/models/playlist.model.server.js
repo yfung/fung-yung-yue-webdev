60086b012d853a1a173f3e64f9f8d34582cf8ed5
Yung-Yue Fung <yfung@toasttab.com>
2017-08-12 20:13:14 -0400

    playlist model

--- assignment/models/playlist.model.server.js
+++ assignment/models/playlist.model.server.js
@@ -1,9 +1,16 @@
 var mongoose = require("mongoose");
 var playlistSchema = require("./playlist.schema.server");
 var playlistModel = mongoose.model("PlaylistModel", playlistSchema);
+playlistModel.createPlaylist = createPlaylist;
+playlistModel.deletePlaylist = deletePlaylist;
 
 module.exports = playlistModel;
 
 function createPlaylist(playlist) {
-    return playlistModel.create(playlist);
+    return playlistModel.create(playlist)
+        .populate("songs");
+}
+
+function deletePlaylist(playlistId) {
+    return playlistModel.remove({_id: playlistId});
 }
