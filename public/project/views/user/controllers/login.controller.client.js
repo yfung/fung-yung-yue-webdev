6c56916dce9ae2b9b7dc1675ac738ebacb7fc2b8
Yung-Yue Fung <yfung@toasttab.com>
2017-08-12 21:28:18 -0400

    syntax

--- public/project/views/user/controllers/login.controller.client.js
+++ public/project/views/user/controllers/login.controller.client.js
@@ -21,7 +21,7 @@
                 .then(function (response) {
                     user = response.data;
                     if (user.length === 0) {
-                        model.errorMessage = "Username or password was incorrect. Please try again!"
+                        model.errorMessage = "Username or password was incorrect. Please try again!";
                     } else {
                         $rootScope.currentUser = user;
                         $location.url("profile/" + user._id);
