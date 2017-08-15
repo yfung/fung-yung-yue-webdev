ff6b07a78125f5ca7ccda87393bd106c921444d8
Yung-Yue Fung <yfung@toasttab.com>
2017-08-15 19:57:57 -0400

    passport 1

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var app = require('./express');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 var express = app.express;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
